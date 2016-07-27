30ceec165769e17e385a0c90962f0c9e94d4b7a2
zhouliang <zhouliang@jiashuangkuaizi.com>
2016-07-27 14:30:29 +0800

    deploy scripts

---
+++ deploy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#coding=utf-8
+#date:2016-07-27
+
+import paramiko
+import time
+
+def _deploy(ip,username,passwd,cmd):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,username,passwd,timeout=5)
+        stdin,stdout,stderr = ssh.exec_command(cmd)
+        print stdout.read()
+        print '%s\tOK\n'%(ip)
+        ssh.close()
+    except:
+        print '%s\tError\n'%(ip)
+
+
+def _put():
+    iplist = ['192.168.1.100','192.168.1.101']
+    for i in iplist:
+        _perip = i
+        scp=paramiko.Transport((_perip,22))
+        scp.connect(username='root',password='123456')
+        sftp=paramiko.SFTPClient.from_transport(scp)
+        sftp.put('/root/mobile.zip','/data/m.xxxx.com/mobile.zip')
+        scp.close()
+
+_put()
+print '##################上传完成####################################'
+time.sleep(3)
+
+print '##################开始备份####################################'
+_deploy("123.57.48.60","root","123456",'cd /data/m.xxxx.com;tar czf bak/mobile.`date "+%Y-%m-%d-%M"`.tar.gz mobile')
+_deploy("123.57.0.49","root","123456",'cd /data/m.xxxx.com;tar czf bak/mobile.`date "+%Y-%m-%d-%M"`.tar.gz mobile')
+print '##################备份完成，开始部署#############################'
+_deploy("123.57.48.60","root","123456",'cd /data/m.xxxx.com;unzip -o mobile.zip')
+_deploy("123.57.0.49","root","123456",'cd /data/m.xxxx.com;unzip -o mobile.zip')
+print '##################部署完成，请验证#############################'
+_deploy("123.57.48.60","root","123456",'cd /data/m.xxxx.com;rm -f mobile.zip')
+_deploy("123.57.0.49","root","123456",'cd /data/m.xxxx.com;rm -f mobile.zip')
+print '##################清理软件包完成#############################'
